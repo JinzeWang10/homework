38142ad68cb44905834d41d89127f5745e13ec90
Jinze Wang <wangjz@seas.upenn.edu>
2023-02-07 21:41:37 -0500

    homework2.6

--- assignments/week3/model_factory.py
+++ assignments/week3/model_factory.py
@@ -18,7 +18,7 @@ def create_model(input_dim: int, output_dim: int) -> MLP:
 
     return MLP(
         input_dim,
-        256,
+        512,
         output_dim,
         2,
         torch.nn.functional.softmax,
