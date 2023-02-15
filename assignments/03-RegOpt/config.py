e18af106d73d8beb88df9380cf117f289ddbc02b
Jinze Wang <wangjz@seas.upenn.edu>
2023-02-14 23:21:02 -0500

    homework3.2

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -7,14 +7,14 @@ from torchvision.transforms import Compose, ToTensor
 
 class CONFIG:
     batch_size = 64
-    num_epochs = 15
-    initial_learning_rate = 0.002
+    num_epochs = 20
+    initial_learning_rate = 0.001
     initial_weight_decay = 0
 
     lrs_kwargs = {
         # You can pass arguments to the learning rate scheduler
         # constructor here.
-        "step_size": 50,
+        "step_size": 64,
         "gamma": 0.99,
     }
 
