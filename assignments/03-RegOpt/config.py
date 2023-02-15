dea4d47610c67c4e3ecfa5af1e18ea35d17f80fb
Jinze Wang <wangjz@seas.upenn.edu>
2023-02-14 22:42:31 -0500

    homework3.2

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -8,7 +8,7 @@ from torchvision.transforms import Compose, ToTensor
 class CONFIG:
     batch_size = 64
     num_epochs = 15
-    initial_learning_rate = 0.003
+    initial_learning_rate = 0.002
     initial_weight_decay = 0
 
     lrs_kwargs = {
