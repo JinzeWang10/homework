b9e414acf83146693e1ac5371ecfc147aae0ae91
Jinze Wang <wangjz@seas.upenn.edu>
2023-02-14 22:24:14 -0500

    homework3.1

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -2,25 +2,32 @@ from typing import Callable
 import torch
 import torch.optim
 import torch.nn as nn
-from torchvision.transforms import Compose, Normalize, ToTensor
+from torchvision.transforms import Compose, ToTensor
 
 
 class CONFIG:
+    """
+    config file
+    """
 
     batch_size = 64
-    num_epochs = 2
-    initial_learning_rate = 0.001
+    num_epochs = 10
+    initial_learning_rate = 0.005
     initial_weight_decay = 0
 
     lrs_kwargs = {
         # You can pass arguments to the learning rate scheduler
         # constructor here.
+        "step_size": 50,
+        "gamma": 0.95,
     }
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.Adam(
-        model.parameters(), lr=CONFIG.initial_learning_rate, weight_decay=CONFIG.initial_weight_decay,
+        model.parameters(),
+        lr=CONFIG.initial_learning_rate,
+        weight_decay=CONFIG.initial_weight_decay,
     )
 
     transforms = Compose(
