85c46b02d91ca39251865bca136073bc09c1e956
Jinze Wang <wangjz@seas.upenn.edu>
2023-03-18 13:26:23 -0400

    hw4

--- assignments/04-Convs/config.py
+++ assignments/04-Convs/config.py
@@ -6,14 +6,11 @@ from torchvision.transforms import Compose, ToTensor
 
 
 class CONFIG:
-    batch_size = 80
+    batch_size = 64
     num_epochs = 4
-
-    initial_learning_rate = 0.0011
-    initial_weight_decay = 0
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
-    ] = lambda model: torch.optim.Adam(model.parameters(), lr=0.001)
+    ] = lambda model: torch.optim.Adam(model.parameters(), lr=0.0011)
 
     transforms = Compose([ToTensor()])
--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -11,13 +11,15 @@ class Model(torch.nn.Module):
         init the model
         """
         super(Model, self).__init__()
-        self.linear_size = 128
-        self.conv1 = torch.nn.Conv2d(num_channels, 32, 2, 2)
+        self.conv1 = torch.nn.Conv2d(num_channels, 32, 3, 3)
+        # self.conv2 = torch.nn.Conv2d(16, 8, 1, 1)
         # self.dropout1 = torch.nn.Dropout(0.1)
-        self.dropout2 = torch.nn.Dropout(0.1)
+        # self.dropout2 = torch.nn.Dropout(0.1)
         self.batch_norm1 = torch.nn.BatchNorm2d(num_features=32)
-        self.fc1 = torch.nn.Linear(800, self.linear_size)
-        self.fc2 = torch.nn.Linear(self.linear_size, num_classes)
+        # self.batch_norm2 = torch.nn.BatchNorm2d(num_features=8)
+
+        self.fc1 = torch.nn.Linear(3200, 64)
+        self.fc2 = torch.nn.Linear(64, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
@@ -30,13 +32,12 @@ class Model(torch.nn.Module):
         # x = torch.nn.functional.relu(x)
         # x = self.dropout1(x)
         # x = self.batch_norm2(x)
-        x = torch.nn.functional.avg_pool2d(x, 3)
+        # x = torch.nn.functional.max_pool2d(x, 3)
         x = torch.flatten(x, 1)
         # print(x.shape)
-        x = self.dropout2(x)
+        # x = self.dropout2(x)
         x = self.fc1(x)
         x = torch.nn.functional.relu(x)
 
-        x = self.fc2(x)
+        x = torch.nn.functional.relu(self.fc2(x))
         # print(x.shape)
-        return x
