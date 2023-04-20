688a56edbd251f689b2c2a6cd30863a165ba7e07
Jinze Wang <wangjz@seas.upenn.edu>
2023-04-20 15:44:23 -0400

    hw6

--- assignments/05-RL/customagent.py
+++ assignments/05-RL/customagent.py
@@ -4,18 +4,34 @@ import torch.nn as nn
 import torch
 import torch.nn.functional as F
 
+
 class Net(nn.Module):
-    def __init__(self, states_dims, n_actions):
+    """
+    Disallow missing docstrings.
+
+    """
+
+    def __init__(self, states_dims: int, n_actions: int) -> None:
+        """
+        Disallow missing docstrings.
+
+        """
         super().__init__()
         self.fc1 = nn.Linear(in_features=states_dims, out_features=64)
         self.fc2 = nn.Linear(in_features=64, out_features=16)
         self.fc3 = nn.Linear(in_features=16, out_features=n_actions)
-    def forward(self, x):
-        x=torch.from_numpy(x)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Disallow missing docstrings.
+
+        """
+        x = torch.from_numpy(x)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x= F.softmax(self.fc3(x),dim=0)
+        x = F.softmax(self.fc3(x), dim=0)
         return x
+
 
 # class Replay:
 #     def __init__(self, max_size):
@@ -51,40 +67,52 @@ class Net(nn.Module):
 
 #         return state_batch, action_batch, reward_batch, new_state_batch, done_batch
 
+
 class Agent:
+    """
+    Disallow missing docstrings.
+
+    """
+
     def __init__(
         self, action_space: gym.spaces.Discrete, observation_space: gym.spaces.Box
     ):
+        """
+        Disallow missing docstrings.
+
+        """
         self.epsilon = 0.3
         self.batch_size = 64
         self.epsilon_decay = 0.001
         self.epsilon_final = 0.01
-        self.lr=0.00075
-        self.discount=0.9
+        self.lr = 0.00075
+        self.discount = 0.9
         self.action_space = action_space
         self.observation_space = observation_space
-        self.update_rate=128
+        self.update_rate = 128
 
-        self.net=Net(8,4)
+        self.net = Net(8, 4)
         # self.target_net=Net(8,4)
-        self.optimizer=torch.optim.Adam(self.net.parameters(),self.lr)
+        self.optimizer = torch.optim.Adam(self.net.parameters(), self.lr)
 
         # self.replay_buffer=Replay(5000)
 
     def act(self, observation: gym.spaces.Box) -> gym.spaces.Discrete:
-        
+        """
+        Disallow missing docstrings.
 
-        if np.random.uniform(0, 1)<self.epsilon:
-            action=self.action_space.sample()
+        """
+
+        if np.random.uniform(0, 1) < self.epsilon:
+            action = self.action_space.sample()
         else:
             with torch.no_grad():
-                actions=self.net(torch.from_numpy(observation))
-            action=np.argmax(actions)
+                actions = self.net(torch.from_numpy(observation))
+            action = np.argmax(actions)
 
         # self.replay_buffer.update1(observation,action)
-        self.state=observation
+        self.state = observation
         return action
-        
 
     def learn(
         self,
@@ -93,7 +121,10 @@ class Agent:
         terminated: bool,
         truncated: bool,
     ) -> None:
-        
+        """
+        Disallow missing docstrings.
+
+        """
 
         # store new sample to replay buffer
         # self.replay_buffer.update2(reward,observation,terminated or truncated)
@@ -102,12 +133,12 @@ class Agent:
         #     return
         # s_batch,a_batch,r_batch,next_s_batch,terminated_batch=self.replay_buffer.sample_batch(self.batch_size)
 
-        q_pred=self.net(self.state).max(dim=1)
-        q_next_max=torch.max(self.net(observation),1)
-        q_target=reward+self.discount*q_next_max*(1-terminated)
+        q_pred = self.net(self.state).max(dim=1)
+        q_next_max = torch.max(self.net(observation), 1)
+        q_target = reward + self.discount * q_next_max * (1 - terminated)
 
-        L=nn.CrossEntropyLoss()
-        loss=L(q_pred, q_target)
+        L = nn.CrossEntropyLoss()
+        loss = L(q_pred, q_target)
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
