3dad51252cdd7a3d461ab74c0f1f1fed175d5caf
Jinze Wang <wangjz@seas.upenn.edu>
2023-04-23 19:21:49 -0400

    test

--- assignments/05-RL/main.py
+++ assignments/05-RL/main.py
@@ -1,7 +1,8 @@
+"""
+"""
+
 import gymnasium as gym
 from customagent import Agent
-
-# import time
 
 SHOW_ANIMATIONS = True
 
@@ -17,15 +18,21 @@ total_reward = 0
 last_n_rewards = []
 for _ in range(100000):
     action = agent.act(observation)
-    observation, reward, terminated, _ = env.step(action)
-    agent.learn(observation, reward, terminated)
+    observation, reward, terminated, truncated, info = env.step(action)
+    agent.learn(observation, reward, terminated, truncated)
     total_reward += reward
 
-    if terminated:
+    if terminated or truncated:
         observation, info = env.reset()
         last_n_rewards.append(total_reward)
         n = min(30, len(last_n_rewards))
         avg = sum(last_n_rewards[-n:]) / n
+        improvement_emoji = "🔥" if (total_reward > avg) else "😢"
+        print(
+            f"{improvement_emoji} Finished with reward {int(total_reward)}.\tAverage of last {n}: {int(avg)}"
+        )
+        if avg > 0:
+            print("🎉 Nice work! You're ready to submit the leaderboard! 🎉")
         total_reward = 0
 
 env.close()
