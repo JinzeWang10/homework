6b29829aa5aa6353fe6c6917b48e27ff3533f19c
Jinze Wang <wangjz@seas.upenn.edu>
2023-04-23 19:14:29 -0400

    test

--- assignments/05-RL/customagent.py
+++ assignments/05-RL/customagent.py
@@ -5,32 +5,32 @@ import torch
 import torch.nn.functional as F
 
 
-class Net(nn.Module):
-    """
-    Disallow missing docstrings.
+# class Net(nn.Module):
+#     """
+#     Disallow missing docstrings.
 
-    """
+#     """
 
-    def __init__(self, states_dims: int, n_actions: int) -> None:
-        """
-        Disallow missing docstrings.
+#     def __init__(self, states_dims: int, n_actions: int) -> None:
+#         """
+#         Disallow missing docstrings.
 
-        """
-        super().__init__()
-        self.fc1 = nn.Linear(in_features=states_dims, out_features=64)
-        self.fc2 = nn.Linear(in_features=64, out_features=16)
-        self.fc3 = nn.Linear(in_features=16, out_features=n_actions)
+#         """
+#         super().__init__()
+#         self.fc1 = nn.Linear(in_features=states_dims, out_features=64)
+#         self.fc2 = nn.Linear(in_features=64, out_features=16)
+#         self.fc3 = nn.Linear(in_features=16, out_features=n_actions)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """
-        Disallow missing docstrings.
+#     def forward(self, x: torch.Tensor) -> torch.Tensor:
+#         """
+#         Disallow missing docstrings.
 
-        """
-        x = torch.from_numpy(x)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.softmax(self.fc3(x), dim=0)
-        return x
+#         """
+#         x = torch.from_numpy(x)
+#         x = F.relu(self.fc1(x))
+#         x = F.relu(self.fc2(x))
+#         x = F.softmax(self.fc3(x), dim=0)
+#         return x
 
 
 class Agent:
@@ -46,34 +46,35 @@ class Agent:
         Disallow missing docstrings.
 
         """
-        self.epsilon = 0.3
-        self.batch_size = 64
-        self.epsilon_decay = 0.001
-        self.epsilon_final = 0.01
-        self.lr = 0.001
-        self.discount = 0.9
-        self.action_space = action_space
-        self.observation_space = observation_space
-        self.update_rate = 128
+        return
+        # self.epsilon = 0.3
+        # self.batch_size = 64
+        # self.epsilon_decay = 0.001
+        # self.epsilon_final = 0.01
+        # self.lr = 0.001
+        # self.discount = 0.9
+        # self.action_space = action_space
+        # self.observation_space = observation_space
+        # self.update_rate = 128
 
-        self.net = Net(8, 4)
-        self.optimizer = torch.optim.Adam(self.net.parameters(), self.lr)
+        # self.net = Net(8, 4)
+        # self.optimizer = torch.optim.Adam(self.net.parameters(), self.lr)
 
     def act(self, observation: gym.spaces.Box) -> gym.spaces.Discrete:
         """
         Disallow missing docstrings.
 
         """
+        return 0
+        # if np.random.uniform(0, 1) < self.epsilon:
+        #     action = self.action_space.sample()
+        # else:
+        #     with torch.no_grad():
+        #         actions = self.net(torch.from_numpy(observation))
+        #     action = np.argmax(actions)
 
-        if np.random.uniform(0, 1) < self.epsilon:
-            action = self.action_space.sample()
-        else:
-            with torch.no_grad():
-                actions = self.net(torch.from_numpy(observation))
-            action = np.argmax(actions)
-
-        self.state = observation
-        return action
+        # self.state = observation
+        # return action
 
     def learn(
         self,
@@ -85,14 +86,14 @@ class Agent:
         Disallow missing docstrings.
 
         """
-        q_pred = self.net(self.state).max(dim=1)
-        q_next_max = torch.max(self.net(observation), 1)
-        q_target = reward + self.discount * q_next_max * (1 - terminated)
+        # q_pred = self.net(self.state).max(dim=1)
+        # q_next_max = torch.max(self.net(observation), 1)
+        # q_target = reward + self.discount * q_next_max * (1 - terminated)
 
-        L = nn.CrossEntropyLoss()
-        loss = L(q_pred, q_target)
-        self.optimizer.zero_grad()
-        loss.backward()
-        self.optimizer.step()
+        # L = nn.CrossEntropyLoss()
+        # loss = L(q_pred, q_target)
+        # self.optimizer.zero_grad()
+        # loss.backward()
+        # self.optimizer.step()
 
         return
